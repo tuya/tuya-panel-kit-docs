8a378d5b1dc9f08bd4ab78ffd15a025f2feda8f2
saber2pr <saber2pr@gmail.com>
2021-07-02 18:21:36 +0800

    fix: qrcode

--- .umirc.ts
+++ .umirc.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   logo:
     'https://imagesd.tuyaus.com/tyims/rms-static/c118f100-7bd1-11ea-be8a-afe9921c3d8a-1586595140625.ico?tyName=tuya_favicon.ico',
   themeConfig: {
-    qrcode: 'tuyaSmart--addVirtualDev?productId=mvhcrizelobov3dw&token=release_common_component',
+    qrcode: 'tuyaSmart--addVirtualDev?productId=oI8uWxJZERgZNpon&token=release_common_component',
     apiData: 'https://cdn.jsdelivr.net/npm/tuya-panel-kit-props-data/props.json',
     demoUrl:
       process.env.NODE_ENV === 'development'
