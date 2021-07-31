a43be378e5ece12afc04809de9d85f01254e485e
Konpaku Youmu <muhai.li@tuya.com>
2021-07-31 12:12:10 +0800

    feat: changelog autofetch (#29)

---
+++ scripts/jobs/render-shields.ts
@@ -0,0 +1,40 @@
+import cheerio from 'cheerio';
+import { resolve } from 'path';
+
+import { sys } from '../sys';
+
+const parseSvg = async (url: string) => {
+  const html = await sys.get(url);
+  const $ = cheerio.load(html);
+  const label =
+    $('svg > g:last-child > text:first-child').text() ||
+    $('svg > g:nth-child(4) > text:first-child').text();
+  const value =
+    $('svg > g:last-child > text:last-child').text() ||
+    $('svg > g:nth-child(4) > text:last-child').text();
+  return {
+    label,
+    value,
+  };
+};
+
+
+const list = [
+  'https://img.shields.io/npm/v/tuya-panel-kit/latest.svg',
+  'https://img.shields.io/github/license/tuya/tuya-panel-kit.svg',
+  'https://img.shields.io/badge/commitizen-friendly-brightgreen.svg?maxAge=2592000',
+  'https://img.shields.io/badge/Conventional%20Commits-1.0.0-brightgreen.svg?maxAge=2592000',
+  'https://codecov.io/gh/tuya/tuya-panel-kit/branch/master/graph/badge.svg',
+];
+
+const dest = resolve(__dirname, '../../public/shields.json');
+
+export const renderShields = async () => {
+  const result = await Promise.all(list.map(parseSvg));
+  await sys.writeFile(
+    dest,
+    JSON.stringify(
+      result.reduce((acc, item) => ({ ...acc, [item.label]: item }), {}),
+    ),
+  );
+}
---
+++ scripts/sys.ts
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import https from 'https';
+import { promisify } from 'util';
+
+export namespace sys {
+  export const writeFile = promisify(fs.writeFile);
+  export const readFile = promisify(fs.readFile)
+  const mkdir = promisify(fs.mkdir)
+
+  export const mkDirPath = async (path: string): Promise<void> => {
+    if (fs.existsSync(path)) {
+      return
+    } else {
+      await mkdir(path)
+    }
+  }
+
+  export function get(url: string): Promise<string> {
+    return new Promise((resolve, reject) =>
+      https
+        .get(url, res => {
+          let str = '';
+          res.on('data', chunk => (str += chunk));
+          res.on('end', () => resolve(str));
+        })
+        .on('error', err => reject(err)),
+    );
+  }
+
+  export const download = async (href: string, dest: string) => {
+    const str = await get(href)
+    await writeFile(dest, str)
+  }
+
+  let stack = 0;
+  export async function jobRunner(job: () => any) {
+    const runner = async () => {
+      try {
+        await job()
+      } catch (error) {
+        if (stack > 10) {
+          throw error;
+        }
+        console.log(`Retry(${stack}), because:`, error);
+        stack++;
+        await runner()
+      }
+    }
+    await runner()
+    stack = 0
+  }
+
+  export async function pipeline(jobs: { [jobName: string]: Function }) {
+    const jobNames = Object.keys(jobs)
+    const jobCount = jobNames.length
+    await jobNames.reduce((prev, key, i) => prev.then(() => {
+      console.log(`[${i + 1}/${jobCount}] ${key.toUpperCase()}...`)
+      return jobs[key]()
+    }), Promise.resolve())
+  }
+
+  // pipe
+  export const compose = <T>(...fns: Array<(value: T) => T>) =>
+    (value: T) => fns.reverse().reduce((a, b) => b(a), value)
+}
