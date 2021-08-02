fb705c9b2edf0e7fe21b41f9b9980d7bcf03072d
saber2pr <saber2pr@gmail.com>
2021-08-02 12:13:57 +0800

    refact: changelog formater

--- scripts/sys.ts
+++ scripts/sys.ts
@@ -4,18 +4,18 @@ import { promisify } from 'util';
 
 export namespace sys {
   export const writeFile = promisify(fs.writeFile);
-  export const readFile = promisify(fs.readFile)
-  const mkdir = promisify(fs.mkdir)
+  export const readFile = promisify(fs.readFile);
+  const mkdir = promisify(fs.mkdir);
 
-  export const log = console.log
+  export const log = console.log;
 
   export const mkDirPath = async (path: string): Promise<void> => {
     if (fs.existsSync(path)) {
-      return
+      return;
     } else {
-      await mkdir(path)
+      await mkdir(path);
     }
-  }
+  };
 
   export function get(url: string): Promise<string> {
     return new Promise((resolve, reject) =>
@@ -30,38 +30,52 @@ export namespace sys {
   }
 
   export const download = async (href: string, dest: string) => {
-    const str = await get(href)
-    await writeFile(dest, str)
-  }
+    const str = await get(href);
+    await writeFile(dest, str);
+  };
 
   let stack = 0;
   export async function jobRunner(job: () => any) {
     const runner = async () => {
       try {
-        await job()
+        await job();
       } catch (error) {
         if (stack > 10) {
           throw error;
         }
         console.log(`Retry(${stack}), because:`, error);
         stack++;
-        await runner()
+        await runner();
       }
-    }
-    await runner()
-    stack = 0
+    };
+    await runner();
+    stack = 0;
   }
 
   export async function pipeline(jobs: { [jobName: string]: Function }) {
-    const jobNames = Object.keys(jobs)
-    const jobCount = jobNames.length
-    await jobNames.reduce((prev, key, i) => prev.then(() => {
-      console.log(`[${i + 1}/${jobCount}] ${key.toUpperCase()}...`)
-      return jobs[key]()
-    }), Promise.resolve())
+    const jobNames = Object.keys(jobs);
+    const jobCount = jobNames.length;
+    await jobNames.reduce(
+      (prev, key, i) =>
+        prev.then(() => {
+          console.log(`[${i + 1}/${jobCount}] ${key.toUpperCase()}...`);
+          return jobs[key]();
+        }),
+      Promise.resolve(),
+    );
   }
 
-  // pipe
   export const compose = <T>(...fns: Array<(value: T) => T>) =>
-    (value: T) => fns.reverse().reduce((a, b) => b(a), value)
+    fns.reduce((a, b) => value => a(b(value)));
+  export const pipe = <T>(...fns: Array<(value: T) => T>) =>
+    compose(...fns.slice().reverse());
+
+  export const combineReplacers = (
+    replacers: Array<[RegExp, (substring: string, ...args: any[]) => string]>,
+  ) =>
+    pipe<string>(
+      ...replacers.map(([reg, replacer]) => (str: string) =>
+        str.replace(new RegExp(reg.source, 'g'), replacer),
+      ),
+    );
 }
