4cb993375697ccda198cdf1a19309bce89046e97
saber2pr <saber2pr@gmail.com>
2021-08-02 12:13:57 +0800

    refact: changelog formater

--- scripts/jobs/fetch-changelog.ts
+++ scripts/jobs/fetch-changelog.ts
@@ -13,33 +13,28 @@ const replaceContent = async (path: string, replacer: (old: string) => string) =
 
 // insert changelog
 const reg = /<Changelog>[\s\S]*?<\/Changelog>/g
-const replaceChangelog = (content: string, value: string) => content.replace(reg, `<Changelog>\n\n${value}\n\n</Changelog>`)
+const replaceChangelog = (value: string) => (content: string) => content.replace(reg, `<Changelog>\n\n${value}\n\n</Changelog>`)
 
-// clear changelog content
-const changeTitle = /# Change Log[\s\S]*?##/g
-const clearContent = (content: string) => content.replace(changeTitle, '##')
-
-// format no-link date title
-const dateTitle = /##\s*?(\d+\.\d+\.\d+)\s*?\((\d{4}-\d{2}-\d{2})\)/g
-const formatErrorDateTitle = (content: string) => content.replace(dateTitle, (_, version, date) => `## [${version}](#) (${date})`)
-
-// format h level
-const h1Title = /\n# \[(\d+\.\d+\.\d+\S*?)\]\(/g
-const formatH1Title = (content: string) => content.replace(h1Title, (_, version) => `\n## [${version}](`)
-
-// format h levels
-const formatHLevels = (content: string) => content.replace('####', '##').replace('#####', '###')
-
-// format no link rc title
-const noLinkRcTitle = /# (\d+\.\d+\.\d+-rc\.\d+)\s*?\((\d{4}-\d{2}-\d{2})\)/g
-const formatRcTitle = (content: string) => content.replace(noLinkRcTitle, (_, version, date) => `## [${version}](#) (${date})`)
-
-// resolve changelog
-const resolveChangelog = sys.compose(clearContent, formatErrorDateTitle, formatH1Title, formatHLevels, formatRcTitle)
+const formatChangelog = sys.combineReplacers([
+  // clear changelog content
+  [/# Change Log[\s\S]*?##/, () => '##'],
+  // format no-link date title
+  [/##\s*?(\d+\.\d+\.\d+)\s*?\((\d{4}-\d{2}-\d{2})\)/, (_, version, date) => `## [${version}](#) (${date})`],
+  // format h level
+  [/\n# \[(\d+\.\d+\.\d+\S*?)\]\(/, (_, version) => `\n## [${version}](`],
+  // format h levels
+  [/####/, () => '##'],
+  [/#####/, () => '###'],
+  // format no link rc title
+  [/# (\d+\.\d+\.\d+-rc\.\d+)\s*?\((\d{4}-\d{2}-\d{2})\)/, (_, version, date) => `## [${version}](#) (${date})`]
+])
 
 // job
 export const downloadChangelog = async () => {
   const upstreamChangelog = await sys.get(changelogHref)
-  await replaceContent(`${changelogPath}.md`, old => resolveChangelog(replaceChangelog(old, upstreamChangelog)))
-  await replaceContent(`${changelogPath}.en.md`, old => resolveChangelog(replaceChangelog(old, upstreamChangelog)))
+  const replacer = replaceChangelog(upstreamChangelog)
+  const transformer = sys.compose(formatChangelog, replacer)
+
+  await replaceContent(`${changelogPath}.md`, transformer)
+  await replaceContent(`${changelogPath}.en.md`, transformer)
 }
