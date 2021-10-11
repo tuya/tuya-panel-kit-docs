866f09b236bb62d083158584db4dd213d891b8f3
muhai <muhai.li@tuya.com>
2021-10-11 14:20:57 +0800

    chore: update

--- public/js/HEARTBEAT.js
+++ public/js/HEARTBEAT.js
@@ -1,30 +1,33 @@
 var HEARTBEAT = {
   ref: 0,
-  delta: 100, // 心跳频率
-  onBeat: () => {},
-  connect() {
-    if (top === self) {
-      // 接收端
-      window.addEventListener('message', event => {
-        const data = event.data || {};
-        if (data.type === 'HEARTBEAT') {
-          HEARTBEAT.ref = data.value;
-        }
+  delta: 500, // 心跳频率
+  connect(iframe, onBeat) {
+    if (iframe && onBeat) {
+      iframe.addEventListener('load', () => {
+        // 接收端
+        window.addEventListener('message', event => {
+          const data = event.data || {};
+          if (data.type === 'HEARTBEAT') {
+            HEARTBEAT.ref = data.value;
+          }
+        });
+        // 检测是否收到
+        setInterval(
+          state => {
+            onBeat(HEARTBEAT.ref !== state.ref);
+            state.ref = HEARTBEAT.ref;
+          },
+          HEARTBEAT.delta,
+          { ref: -1 },
+        );
       });
-      // 检测是否收到
-      setInterval(
-        state => {
-          HEARTBEAT.onBeat(HEARTBEAT.ref !== state.ref);
-          state.ref = HEARTBEAT.ref;
-        },
-        HEARTBEAT.delta,
-        { ref: -1 },
-      );
     } else {
-      setInterval(() => {
-        // 发送心跳包
-        top.postMessage({ type: 'HEARTBEAT', value: Date.now() }, '*');
-      }, HEARTBEAT.delta);
+      if (top !== self) {
+        setInterval(() => {
+          // 发送心跳包
+          top.postMessage({ type: 'HEARTBEAT', value: Date.now() }, '*');
+        }, HEARTBEAT.delta);
+      }
     }
   },
 };
